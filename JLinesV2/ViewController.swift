ad5a79d91cc662cd1f5990c779c62bcd53700873
Jozsef Romhanyi <JozsefRomhanyi@Jozsefs-MacBook-Pro.local>
2015-03-13 13:25:18 +0100

    Initial Commit

---
+++ JLinesV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLinesV2
+//
+//  Created by Jozsef Romhanyi on 13.03.15.
+//  Copyright (c) 2015 Jozsef Romhanyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
